eb0336b213113f14164309d35b70c0efd59e4595
Fabian3050 <137364972+Fabian3050@users.noreply.github.com>
2025-11-22 13:09:57 -0300

    FIX: arreglo en vista materializada

--- Creador_de_tabla.sql
+++ Creador_de_tabla.sql
@@ -283,7 +283,7 @@ SELECT
     date_trunc('day', p.date_hour)::date AS fecha,
     SUM(c.weight) AS peso_total
 FROM pickup p
-JOIN containers c ON p.id_container = c.id
+JOIN container c ON p.id_container = c.id
 GROUP BY fecha
 ORDER BY fecha;
 
